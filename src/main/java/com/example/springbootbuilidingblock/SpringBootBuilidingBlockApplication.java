7966d59c5f8b2ba279aa40485f64ddcbf81d6182
James.Obi <james.obi@interswitchng.com>
2022-10-18 08:27:39 +0100

    setup spring

---
+++ src/main/java/com/example/springbootbuilidingblock/SpringBootBuilidingBlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbuilidingblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuilidingBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuilidingBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootbuilidingblock/SpringBootBuilidingBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbuilidingblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuilidingBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
